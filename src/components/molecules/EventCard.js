f0003cb430a8821d53d9ccbae6c617dcac5353b7
mrvirani <maulikvirani5050@gmail.com>
2025-01-30 13:24:45 +0530

    initial commit

---
+++ src/components/molecules/EventCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EventCard = () => {
+  return (
+    <View>
+      <Text>EventCard</Text>
+    </View>
+  )
+}
+
+export default EventCard
+
+const styles = StyleSheet.create({})
